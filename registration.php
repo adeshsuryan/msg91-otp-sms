479e30c7ae64a2b5ef9ba2cfb0405656ef898f16
Adesh Suryan <adeshsuryan2005@gmail.com>
2019-09-25 20:31:08 +0530

    Initial commit::MSG91@adeshsuryan

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category   adeshsuryan
+ * @package    Adeshsuryan_Msg91SMSOTP
+ * @author     Master Software Solutions
+ * @copyright  Copyright (c) 2009-2019 Adeshsuryan (http://www.adeshsuryan.in/)
+ * @license    http://www.adeshsuryan.in
+ * @contact    adeshsuryan2005@gmail.com
+ */ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Adeshsuryan_Msg91SMSOTP',
+    __DIR__
+);
